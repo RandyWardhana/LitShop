9d013b184851d485d39c7e577b79d05efcbc9085
Adrian M. R <adrianmridwan@gmail.com>
2018-12-04 18:06:34 +0700

    Cleanup repository

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-// // import ejs from 'ejs'
-// // import express from 'express'
-// // import bodyparser from 'body-parser'
-// // import mysql from 'mysql2'
-// import multiparty from 'multiparty'
-
-var ejs = require('ejs')
-var express = require('express')
-var bodyparser = require('body-parser')
-var mysql = require('mysql2')
-var multiparty = require('multiparty')
+import express from 'express'
+import bodyparser from 'body-parser'
 
 const app = express()
+const port = 3000
 
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }))
@@ -18,13 +10,11 @@ app.use(bodyparser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 app.set('views', process.cwd() + '/view')
 
-// Set URL for file
+// Set url for static file
 app.use('/static', express.static(process.cwd() + '/static'))
 
-app.get('/', async(req, res) => { 
+app.get('/', async (req, res) => {
     res.render('index')
 })
 
-app.listen('212', async (e) => {
-    console.log('Listening on Port: 212')
-})
+app.listen(port, () => { console.log(`Listening on port ${port}`) })
